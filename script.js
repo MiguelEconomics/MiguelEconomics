3cde3066008b897883446254f8da611e2eec378a
Miguel Moreno <mianmo39@outlook.com>
2023-05-18 17:00:12 -0500

    Full portfolio development

---
+++ script.js
@@ -0,0 +1,41 @@
+// Header toggle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+
+// Typing Effect
+
+let typed = new Typed('.auto-input',{
+    strings: ['Graduated Economist!', 'Data Scientist!', 'Cat Lover :3'],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true,
+})
+
+// Activate Link State On Scroll
+
+// Get All Links
+let navLinks = document.querySelectorAll('nav ul li a');
+// Get all Sections
+let sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', function () {
+    const scrollPos = window.scrollY + 20;
+
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+});
